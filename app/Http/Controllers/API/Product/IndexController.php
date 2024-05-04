bfcc1b411b37d2a83236e3a58b75b16fdef03de8
Diana Dvoryak <dianadvoryak.w@gmail.com>
2024-05-04 09:48:19 +0200

    filter request

--- app/Http/Controllers/API/Product/IndexController.php
+++ app/Http/Controllers/API/Product/IndexController.php
@@ -15,7 +15,7 @@ class IndexController extends Controller
         $data = $request->validated();
         $filter = app()->make(ProductFilter::class, ['queryParams' => array_filter($data)]);
 
-        $products = Product::filter($filter)->get();
+        $products = Product::filter($filter)->paginate(5, ['*'], 'page', $data['page']);
         return IndexProductResourse::collection($products);
     }
 }
